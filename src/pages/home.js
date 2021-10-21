98e2bae94ee12b810fce394b62caa3fb22901f86
Andreas Larssamils <larssamils.andreas@gmail.com>
2021-10-21 14:58:45 +0800

    add footer and add accordion

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import FaqsContainer from '../containers/faqs';
 import JumbotronContainer from '../containers/jumbotron';
+import FooterContainer from '../containers/footer';
 
-function Home() {
-  return <JumbotronContainer />;
+export default function Home() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
 }
-
-export default Home;
